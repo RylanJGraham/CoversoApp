9c92377e42819a59e5f4e3ebf1f38e626eb92444
Rylan Graham <RylanGraham02@gmail.com>
2025-08-12 21:25:15 +0000

    I dont like the shadow or the highlight you added to the fields input. i

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,18 +6,15 @@ import { cn } from "@/lib/utils"
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
     return (
-      <div className="relative group">
-        <div className="absolute -inset-0.5 bg-gradient-to-b from-primary-gradient-start to-primary-gradient-end rounded-lg blur opacity-0 group-focus-within:opacity-75 transition duration-200"></div>
         <input
           type={type}
           className={cn(
-            "relative flex h-10 w-full rounded-md border border-gray-300 bg-background px-3 py-2 text-base text-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             className
           )}
           ref={ref}
           {...props}
         />
-      </div>
     )
   }
 )
